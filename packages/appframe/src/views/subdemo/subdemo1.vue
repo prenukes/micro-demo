3a252777a386f9d26298b4372e095ca83a4d3faf
hyk <1751090934@qq.com>
2024-07-08 09:23:03 +0800

    init

---
+++ packages/appframe/src/views/subdemo/subdemo1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <micro-app name="subapp" url="http://localhost:82/index" iframe></micro-app>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
